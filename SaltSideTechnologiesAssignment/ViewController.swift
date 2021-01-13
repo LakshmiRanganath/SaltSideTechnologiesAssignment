594623205135e79359b00a2be2476cca854a4a5b
Lakshmi Ranganatha Hema <laki271992@gmail.com>
2021-01-13 10:16:36 -0800

    Initial Commit

---
+++ SaltSideTechnologiesAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaltSideTechnologiesAssignment
+//
+//  Created by Lakshmi Ranganatha Hema on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
