d582ce5319b8701ab89ed1df574dcf24600c2aa3
Lakshmi Ranganatha Hema <laki271992@gmail.com>
2021-01-13 15:16:39 -0800

    #4 : ImageCache from lruCache is created

---
+++ SaltSideTechnologiesAssignment/SaltSideExtensions/SaltsideImageExtension.swift
@@ -0,0 +1,53 @@
+//
+//  SaltsideImageExtension.swift
+//  SaltSideTechnologiesAssignment
+//
+//  Created by Lakshmi Ranganatha Hema on 1/13/21.
+//
+
+import Foundation
+import UIKit
+
+//LRU cache with capacity 5 is initialised
+let imageCache = SaltsideLRUCache<String, Any>(SaltsideConstants.saltsideLRUMaxCacheSize)
+
+class CacheImageView : UIImageView{
+    //This is used  to cross check with urlstring before loading into imageview, to avoid mismatch of images on scroll etc...
+    var imageUrlString  = String()
+    
+    func loadImageViewWithUrlString(urlString : String){
+        imageUrlString = urlString
+        let url = URL(string: urlString)
+        image = nil
+        
+        //Check if the image with urlstring is present in cache, if present fetch from cache else, fetch image from urlString and save in lrucache
+        if imageCache.isValid(key: urlString){
+            self.image = imageCache.get(urlString) as? UIImage
+        }
+        else{
+            URLSession.shared.dataTask(with: url!){(data, response, error) in
+                if error != nil{
+                    print(error?.localizedDescription ?? "Error occured while fetching image from url")
+                    return
+                }
+                if data == nil{
+                    print("No data found")
+                    return
+                }
+                guard let imageToCache = UIImage(data: data!) else{
+                    print("Error occured while fetchimng image from data")
+                    return
+                }
+                imageCache.put(urlString, imageToCache)
+            
+                DispatchQueue.main.async {[weak self] in
+                    if self?.imageUrlString == urlString{
+                        self?.image = imageToCache
+                    }
+                }
+            }.resume()
+        }
+        
+        
+    }
+}
